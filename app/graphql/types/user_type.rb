13ac0ae03f326bf27a96dc2fcb158f2bfd176b0f
jesusMava <jesusmacedo7@gmail.com>
2024-11-02 10:20:17 -0600

    feature: Add new types with graphql(this types are like the model structure)

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String
+    field :email, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :post_count, Integer, null: false
+    field :posts, [Types::PostType], null: false
+  end
+end
